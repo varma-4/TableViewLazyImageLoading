250a96ceba59b967fc58206eb366fc904c1ea6ae
varma-4 <varmadatla444@gmail.com>
2018-06-13 17:55:56 +0530

    CustomTableViewCell designed programatically with help of VFL for setting constraints

--- CodingAssignment/Resources/Extensions.swift
+++ CodingAssignment/Resources/Extensions.swift
@@ -8,10 +8,39 @@
 
 import UIKit
 
+// Caching images to make not download the image 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 extension StringProtocol {
     
     var firstUppercased: String {
         guard let first = first else { return "" }
         return String(first).uppercased() + dropFirst()
+    }
+}
+
+extension UIImageView {
+    
+    func loadImageWithURL(urlString:String) {
+        let url = URL(string: urlString)
+        image = nil
+        
+        if let imageCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = imageCache
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            if error != nil {
+                print("error")
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                let imageToCache = UIImage(data: data!)
+                imageCache.setObject(imageToCache!, forKey: urlString as AnyObject)
+                self.image = imageToCache
+            })
+        }).resume()
+        
     }
 }
--- CodingAssignment/Views/HomeTableViewCell.swift
+++ CodingAssignment/Views/HomeTableViewCell.swift
@@ -9,6 +9,36 @@
 import UIKit
 
 class HomeTableViewCell: UITableViewCell {
+    
+    var itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .red
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        return imageView
+    }()
+    
+    var itemTitle: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .brown
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = ""
+        return label
+    }()
+    
+    var itemDescription: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.text = ""
+        return label
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,5 +47,36 @@ class HomeTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupViews() {
+        addSubview(itemImageView)
+        addSubview(itemTitle)
+        addSubview(itemDescription)
+        
+        setConstraints()
+    }
+    
+    func setConstraints() {
+        // ItemImageView Contraints
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[v0(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":itemImageView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10.0@1000-[v0(<=50)]-10.0@500-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":itemImageView]))
+        
+        // ItemTitleLabel Constraints
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:v0-10-[v1]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":itemImageView, "v1": itemTitle]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":itemTitle]))
+        
+        // ItemDescriptionLabel Constraints
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "[v0]-10.0-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": itemDescription]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[v0]-10.0@1000-[v1]->=10.0@750-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":itemTitle, "v1": itemDescription]))
+    }
+    
 }
